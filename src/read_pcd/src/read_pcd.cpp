615b4267baa9c32d6a11b613be00bb1146d86d01
Shahab Jafari <133869713+shahabbai@users.noreply.github.com>
2025-01-22 10:46:17 +0330

    added getSortedPCDFiles function for dynamic number of files

--- src/read_pcd/src/read_pcd.cpp
+++ src/read_pcd/src/read_pcd.cpp
@@ -3,72 +3,88 @@
 #include <pcl/point_cloud.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <sensor_msgs/PointCloud2.h>
-#include <pcl/io/pcd_io.h>//which contains the required definitions to load and store point clouds to PCD and other file formats.
-#include <string>     
+#include <pcl/io/pcd_io.h>
+#include <filesystem>
+#include <vector>
+#include <algorithm>
+#include <string>
 #include <iostream>
 #include <fstream>
+
+namespace fs = std::filesystem;
+
 using namespace std;
- 
-sensor_msgs::PointCloud2 read_pcd_path(string path, int num)
-{
-  pcl::PointCloud<pcl::PointXYZ> cloud;
-  sensor_msgs::PointCloud2 output;
-  string path_final = path + to_string(num) + ".pcd";
-  cout << "Read file:" << path_final << endl; 
-  pcl::io::loadPCDFile (path_final, cloud); //修改自己pcd文件所在路径
-  //Convert the cloud to ROS message
-  
-  pcl::toROSMsg(cloud, output);
-  output.header.frame_id = "vlp16";//this has been done in order to be able to visualize our PointCloud2 message on the RViz visualizer    
-//！！！这一步需要注意，是后面rviz的 fixed_frame  !!!敲黑板，画重点。
-  return output;
+
+// Function to get sorted PCD file paths
+vector<string> getSortedPCDFiles(const string& directory) {
+    vector<string> pcd_files;
+    for (const auto& entry : fs::directory_iterator(directory)) {
+        if (entry.path().extension() == ".pcd") {
+            pcd_files.push_back(entry.path().string());
+        }
+    }
+    // Sort files alphabetically
+    sort(pcd_files.begin(), pcd_files.end());
+    return pcd_files;
 }
 
+// Function to read a PCD file and convert it to a ROS PointCloud2 message
+sensor_msgs::PointCloud2 readPCDFile(const string& filepath) {
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+    sensor_msgs::PointCloud2 output;
 
+    cout << "Reading file: " << filepath << endl;
 
+    if (pcl::io::loadPCDFile(filepath, cloud) == -1) {
+        cerr << "Error loading file: " << filepath << endl;
+        return output;
+    }
 
-int main (int argc, char **argv)
-{
-  ros::init (argc, argv, "UandBdetect");
-  ros::NodeHandle nh;
-  ros::Publisher pcl_pub = nh.advertise<sensor_msgs::PointCloud2> ("pcl_output", 10);
-  pcl::PointCloud<pcl::PointXYZ> cloud;
-  sensor_msgs::PointCloud2 output;
-  string path = "/home/ysy/pcd_mod/vlp16_";
-  string bagname = "/home/ysy/mod.bag";
-
-  ofstream fileout(bagname,ios::trunc);//ios::trunc是清除原文件内容,可不写,默认就是它
-  if(!fileout){
-      cout << "Create file failure...\n";
-      exit(0);
-  }
-  fileout.close();
-
-  rosbag::Bag bag;
-  double time = ros::Time::now().toSec();
-  bag.open(bagname,rosbag::bagmode::Write);
-
-
-  for (float num = 1. ; num <= 792 ; num++)
-  {
-    output = read_pcd_path(path,num);
-    output.header.frame_id = "vlp16" ;
-    output.header.stamp = ros::Time().fromSec(float(num/10)+time);
-    // cout << "scan time:" << float(num/10)+time << endl; 
-    cout << "scan time:" << output.header.stamp << endl; 
-    bag.write("/velodyne_points",output.header.stamp,output);
-  }
-  bag.close();
-  cout<<"Bag file write finish"<<endl;
-
-  // ros::Rate loop_rate(1);
-  // while (ros::ok())
-  // {
-  //   pcl_pub.publish(output);
-  //   ros::spinOnce();
-  //   loop_rate.sleep();
-  // }
-  return 0;
+    // Convert the cloud to ROS message
+    pcl::toROSMsg(cloud, output);
+    output.header.frame_id = "vlp16"; // Set frame ID for RViz visualization
+    return output;
 }
 
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "UandBdetect");
+    ros::NodeHandle nh;
 
+    string directory = "/mnt/Downloads/cloud_pcd/";
+    string bagname = "/mnt/Downloads/mod.bag";
+
+    // Get sorted list of PCD files
+    vector<string> pcd_files = getSortedPCDFiles(directory);
+    if (pcd_files.empty()) {
+        cerr << "No PCD files found in the specified directory: " << directory << endl;
+        return -1;
+    }
+
+    ofstream fileout(bagname, ios::trunc);
+    if (!fileout) {
+        cerr << "Failed to create file: " << bagname << endl;
+        return -1;
+    }
+    fileout.close();
+
+    rosbag::Bag bag;
+    double time = ros::Time::now().toSec();
+    bag.open(bagname, rosbag::bagmode::Write);
+
+    for (size_t i = 0; i < pcd_files.size(); ++i) {
+        sensor_msgs::PointCloud2 output = readPCDFile(pcd_files[i]);
+        if (output.data.empty()) {
+            cerr << "Skipping invalid file: " << pcd_files[i] << endl;
+            continue;
+        }
+
+        output.header.stamp = ros::Time().fromSec(static_cast<double>(i) / 10.0 + time);
+        cout << "Scan time: " << output.header.stamp << endl;
+        bag.write("/aicc_LidarFrame", output.header.stamp, output);
+    }
+
+    bag.close();
+    cout << "Bag file write complete." << endl;
+
+    return 0;
+}
